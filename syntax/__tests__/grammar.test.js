4205bdcc6c7ddb06dc42f59c6978ae3ab7eb86eb
Ray Toal <rtoal@lmu.edu>
2019-03-04 19:37:58 -0800

    A real test script for the grammar

--- syntax/__tests__/grammar.test.js
+++ syntax/__tests__/grammar.test.js
@@ -1,6 +1,30 @@
-describe('The parser', () => {
-  test('does not crash', (done) => {
-    expect(1).toEqual(1);
-    done();
+/*
+ * Grammar Tests
+ *
+ * These tests check that we’ve constructed our grammar correctly: that
+ * programs that we expect to be matched by the grammar are matched, and
+ * those that we expect not to be matched will cause an error to be thrown.
+ */
+
+const fs = require('fs');
+const parse = require('../parser');
+
+describe('The grammar', () => {
+  fs.readdirSync(__dirname).forEach((name) => {
+    if (name.endsWith('.error.tig')) {
+      test(`detects a syntax error in ${name}`, (done) => {
+        fs.readFile(`${__dirname}/${name}`, 'utf-8', (err, input) => {
+          expect(() => parse(input)).toThrow(/Syntax Error/);
+          done();
+        });
+      });
+    } else if (name.endsWith('.tig')) {
+      test(`matches the program ${name}`, (done) => {
+        fs.readFile(`${__dirname}/${name}`, 'utf-8', (err, input) => {
+          expect(parse(input)).toBeTruthy();
+          done();
+        });
+      });
+    }
   });
 });
