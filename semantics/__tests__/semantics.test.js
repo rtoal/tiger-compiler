c164d9a183b569b043c69b112a9f34b915c96480
Ray Toal <rtoal@lmu.edu>
2019-04-29 22:10:11 -0700

    Get context coverage to 100%

--- semantics/__tests__/semantics.test.js
+++ semantics/__tests__/semantics.test.js
@@ -20,6 +20,7 @@ let
   type list = array of string
   var c: Circle := Circle {y = 2, x = 5<3&2<>1, color = "blue"}
   var dogs: list := list [3] of "woof"
+  function successor(x: int) = x + 1
 in
   dogs[1] := "Sparky";
   for i := 1 to 10 do
