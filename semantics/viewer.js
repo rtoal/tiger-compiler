8ff17866d2303328c16a8c37fba169f6e3d02ae8
Ray Toal <rtoal@lmu.edu>
2020-04-05 20:18:14 -0700

    Alternative semantics viewer

--- semantics/viewer.js
+++ semantics/viewer.js
@@ -1,41 +1,77 @@
-const util = require('util');
-
 module.exports = root => {
-  const entities = new Map();
-  addReachableEntities(root, entities);
-  return [...entities].map(([node, index]) => detailLine(node, index, entities)).join('\n');
+  const dataset = { entities: [], edges: [] };
+  addReachableEntities(root, dataset, -1);
+  dataset.entities = dataset.entities.map(e => nodeText(e));
+  return dataset;
 };
 
-function addReachableEntities(node, entities) {
-  if (node === null || typeof node !== 'object' || entities.has(node)) {
+function nodeText(node) {
+  let text = node.constructor.name;
+  Object.entries(node).forEach(([prop, value]) => {
+    if (['boolean', 'number', 'string'].includes(typeof value)) {
+      text += ` ${prop}=${value}`;
+    }
+  });
+  return text;
+}
+
+function addReachableEntities(node, dataset, parentIndex, label) {
+  if (dataset.entities.includes(node)) {
+    dataset.edges.push({ source: parentIndex, target: dataset.entities.indexOf(node), label });
     return;
   }
-  entities.set(node, entities.size);
-  Object.values(node).forEach(value => {
+
+  dataset.entities.push(node);
+  const nodeIndex = dataset.entities.length - 1;
+  if (parentIndex >= 0) {
+    dataset.edges.push({ source: parentIndex, target: nodeIndex, label });
+  }
+
+  Object.entries(node).forEach(([prop, value]) => {
     if (Array.isArray(value)) {
-      value.forEach(n => addReachableEntities(n, entities));
-    } else {
-      addReachableEntities(value, entities);
+      value.forEach((n, i) => addReachableEntities(n, dataset, nodeIndex, `${prop}[${i}]`));
+    } else if (typeof value === 'object' && value !== null) {
+      addReachableEntities(value, dataset, nodeIndex, prop);
     }
   });
 }
 
-function ref(value, entities) {
-  if (value === undefined || typeof value === 'function') {
-    return undefined;
-  } else if (Array.isArray(value)) {
-    return `[${value.map(v => ref(v, entities))}]`;
-  } else if (typeof value === 'object' && value !== null) {
-    return `#${entities.get(value)}`;
-  }
-  return util.inspect(value);
-}
+// module.exports = root => {
+//   const entities = new Map();
+//   addReachableEntities(root, entities);
+//   return [...entities].map(([node, index]) => detailLine(node, index, entities)).join('\n');
+// };
 
-function detailLine(node, index, entities) {
-  let line = `${index} (${node.constructor.name})`;
-  Object.keys(node).forEach(key => {
-    const value = ref(node[key], entities);
-    line += value === undefined ? '' : ` ${key}=${value}`;
-  });
-  return line;
-}
+// function addReachableEntities(node, entities) {
+//   if (node === null || typeof node !== 'object' || entities.has(node)) {
+//     return;
+//   }
+//   entities.set(node, entities.size);
+//   Object.values(node).forEach(value => {
+//     if (Array.isArray(value)) {
+//       value.forEach(n => addReachableEntities(n, entities));
+//     } else {
+//       addReachableEntities(value, entities);
+//     }
+//   });
+// }
+
+// function ref(value, entities) {
+//   if (value === undefined || typeof value === 'function') {
+//     return undefined;
+//   } else if (Array.isArray(value)) {
+//     return `[${value.map(v => ref(v, entities))}]`;
+//   } else if (typeof value === 'object' && value !== null) {
+//     return `#${entities.get(value)}`;
+//   }
+//   return util.inspect(value);
+// }
+
+// function detailLine(node, index, entities) {
+//   let line = `${index} (${node.constructor.name})`;
+//   Object.keys(node).forEach(key => {
+//     const value = ref(node[key], entities);
+//     line += value === undefined ? '' : ` ${key}=${value}`;
+//   });
+//   return line;
+// }
